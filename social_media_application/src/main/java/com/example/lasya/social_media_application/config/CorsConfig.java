2a0abf2899c70eebc3b5848bea57f16a9de19895
lasyabandi <lasyabandi@gmail.com>
2023-10-24 18:30:59 -0400

    second commit
    1. added CorsConfig
    2. re-tested all user APIs in controller class

---
+++ social_media_application/src/main/java/com/example/lasya/social_media_application/config/CorsConfig.java
@@ -0,0 +1,28 @@
+package com.example.lasya.social_media_application.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+public class CorsConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**") // Adjust the mapping pattern
+                        .allowedOrigins("*") // Allow requests from any origin
+                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+                        .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
--- social_media_application/src/main/java/com/example/lasya/social_media_application/controller/UserController.java
+++ social_media_application/src/main/java/com/example/lasya/social_media_application/controller/UserController.java
@@ -12,11 +12,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.util.List;
 import java.util.Map;
 import java.util.OptionalInt;
 
+@CrossOrigin
 @RestController
 public class UserController {
 
